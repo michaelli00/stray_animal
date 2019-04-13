a26ee892efab9c97bfef48ac432d0c3788a14399
mli25782 <mli25782@gmail.com>
2019-04-13 12:19:38 -0400

    initial commit

---
+++ app/src/main/java/com/example/stray_animal/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.stray_animal;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.esri.arcgisruntime.mapping.ArcGISMap;
+import com.esri.arcgisruntime.mapping.Basemap;
+import com.esri.arcgisruntime.mapping.view.MapView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private MapView mMapView;
+
+    private void setupMap() {
+        if (mMapView != null) {
+            Basemap.Type basemapType = Basemap.Type.STREETS_VECTOR;
+            double latitude = 34.09042;
+            double longitude = -118.71511;
+            int levelOfDetail = 11;
+            ArcGISMap map = new ArcGISMap(basemapType, latitude, longitude, levelOfDetail);
+            mMapView.setMap(map);
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mMapView = findViewById(R.id.mapView);
+        setupMap();
+    }
+
+    @Override
+    protected void onPause() {
+        if (mMapView != null) {
+            mMapView.pause();
+        }
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (mMapView != null) {
+
+            mMapView.resume();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (mMapView != null) {
+            mMapView.dispose();
+        }
+        super.onDestroy();
+    }
+//    FloatingActionButton fab = findViewById(R.id.fab);
+
+
+
+}
